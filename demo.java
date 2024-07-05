2c19936f6879873164bd3a91cf62403b9e336a60
Shantanu9421 <110804946+Shantanu9421@users.noreply.github.com>
2024-07-05 14:21:43 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
-class demo{
-public static void main(String [] args){
-System.out.println("hello world");
-}
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
